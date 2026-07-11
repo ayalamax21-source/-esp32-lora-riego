32cd85524915a10d43a2f8b1a3982cd4052f5b4a
ayalamax21-source <ayalamax.21@gmail.com>
2026-07-10 20:59:49 -0600

    Crear config.h
    Agregar configuración del Nodo

---
+++ Nodo/config.h
@@ -0,0 +1,43 @@
+#ifndef CONFIG_H
+
+#define CONFIG_H
+
+//==============================
+
+// CONFIGURACIÓN DEL NODO
+
+//==============================
+
+#define NODE_ID 1
+
+// Frecuencia LoRa
+
+#define LORA_FREQUENCY 915E6
+
+// Pines LoRa
+
+#define LORA_SCK   18
+
+#define LORA_MISO  19
+
+#define LORA_MOSI  23
+
+#define LORA_SS     5
+
+#define LORA_RST   14
+
+#define LORA_DIO0  26
+
+// Relevadores
+
+#define RELAY1 25
+
+#define RELAY2 33
+
+#define RELAY3 32
+
+#define RELAY4 27
+
+#define VERSION "1.0.0"
+
+#endif
