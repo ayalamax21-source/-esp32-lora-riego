81c6821c7a665627162fb76c3fc126c8e867a6c5
ayalamax21-source <ayalamax.21@gmail.com>
2026-07-10 21:11:22 -0600

    Crear script.js
    Agregar script inicial de la interfaz web

---
+++ web/script.js
@@ -0,0 +1,13 @@
+document.getElementById("nodes").innerHTML = `
+
+<h3>Nodo 1</h3>
+
+<button>Válvula 1</button>
+
+<button>Válvula 2</button>
+
+<button>Válvula 3</button>
+
+<button>Válvula 4</button>
+
+`;
