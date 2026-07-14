8569d9d7177d357d75deac9c4494aed6f8823710
ayalamax21-source <ayalamax.21@gmail.com>
2026-07-13 22:41:42 -0600

    Actualizar protocol.h
    Agregar protocolo compartido entre Maestro y Nodo

--- shared/protocol.h
+++ shared/protocol.h
@@ -1,1 +1,35 @@
-Agregar protocolo compartido entre Maestro y Nodo
+#ifndef PROTOCOL_H
+
+#define PROTOCOL_H
+
+#include <Arduino.h>
+
+#define MSG_MAX_LENGTH 64
+
+enum MessageType {
+
+    MSG_COMMAND = 1,
+
+    MSG_STATUS  = 2,
+
+    MSG_ACK     = 3,
+
+    MSG_ERROR   = 4
+
+};
+
+struct LoRaMessage {
+
+    uint8_t sender;
+
+    uint8_t receiver;
+
+    MessageType type;
+
+    char command[16];
+
+    char value[32];
+
+};
+
+#endif
